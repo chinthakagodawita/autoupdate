5d8db9855975be83766be44b048b050c5f9f138e
Chin Godawita <1452384+chinthakagodawita@users.noreply.github.com>
2020-01-04 17:50:51 +0000

    Add initial update support (#4)

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,41 @@
 #!/usr/bin/env node
 const process = require('process');
+const fs = require('fs');
+
+const ghCore = require('@actions/core');
 
 const AutoUpdater = require('../src/autoupdater.js');
 
 async function main() {
-  updater = new AutoUpdater({
-    githubToken: process.env['GITHUB_TOKEN'],
-    pullRequest: 'PR-1',
-    repository: 'chinthakagodawita/autoupdate-action',
-  });
-  await updater.run();
+  const eventPath = process.env['GITHUB_EVENT_PATH'];
+  const eventName = process.env['GITHUB_EVENT_NAME'];
+
+  const rawEventData = fs.readFileSync(eventPath, 'utf8');
+  const eventData = JSON.parse(rawEventData);
+  
+  ghCore.debug(`EVENT NAME: ${eventName}`);
+  ghCore.debug(`EVENT DATA: ${rawEventData}`);
+
+  const updater = new AutoUpdater(
+    process.env['GITHUB_TOKEN'],
+    eventData,
+    true,
+  );
+
+  if (eventName === 'pull_request') {
+    await updater.handlePullRequest();
+  } else if (eventName === 'push') {
+    await updater.handlePush();
+  } else {
+    throw new Error(
+      `Unknown event type '${eventName}', only 'push' and 'pull_request' are supported.`
+    );
+  }
 }
 
 if (require.main === module) {
   main().catch((e) => {
     process.exitCode = 1;
-    console.log(e);
+    ghCore.setFailed(e.message);
   });
 }
--- src/autoupdater.js
+++ src/autoupdater.js
@@ -1,39 +1,134 @@
 const github = require('@actions/github');
+const ghCore = require('@actions/core');
 
 class AutoUpdater {
-  pullRequest;
-  repoName;
-  repoOwner;
-  octoKit;
+  eventData;
+  runningOnGithub;
+  octokit;
 
-  constructor({
-    githubToken = null,
-    pullRequest = null,
-    repository = null,
-  } = {}) {
-    this.pullRequest = pullRequest;
-    
-    const repoParts = repository.split('/');
-    if (repoParts.length !== 2) {
-      throw new Error(
-        `Invalid format for repository, expected 'owner/repository', got '${repository}'.`
-      )
-    }
-
-    this.repoOwner = repoParts[0];
-    this.repoName = repoParts[1];
+  constructor(githubToken, eventData, runningOnGithub = true) {
+    this.eventData = eventData;
+    this.runningOnGithub = this.runningOnGithub;
 
     if (githubToken !== null && githubToken !== void 0) {
-      this.octoKit = new github.GitHub(githubToken);
+      this.octokit = new github.GitHub(githubToken);
+    } else {
+      throw new Error("Github token was not provided, please check that you have provided the 'GITHUB_TOKEN' environment variable.");
     }
   }
-  
-  async run() {
-    console.log(await this.octoKit.pulls.list({
-      owner: this.repoOwner,
-      repo: this.repoName,
-    }));
-    console.log('running yo')
+
+  async handlePush() {
+    const { ref, repository } = this.eventData;
+
+    ghCore.info(`Handling push event on ref '${ref}'`);
+
+    if (!ref.startsWith('refs/heads/')) {
+      ghCore.warning('Push event was not on a branch, skipping.');
+      return;
+    }
+
+    const baseBranch = ref.replace('refs/heads/', '');
+
+    const pulls = await this.octokit.pulls.list({
+      owner: repository.owner.name,
+      repo: repository.name,
+      base: baseBranch,
+      state: 'open',
+      sort: 'updated',
+      direction: 'desc',
+    });
+
+    if (pulls.data.length === 0) {
+      ghCore.info(`Base branch '${baseBranch}' has no pull requests that point to it, skipping autoupdate.`);
+      return;
+    }
+
+    let updated = 0;
+    for await (const pullsPage of this.octokit.paginate.iterator(pulls)) {
+      for (const pull of pulls.data) {
+        const isUpdated = await this.update(pull);
+        if (isUpdated) {
+          updated++;
+        }
+      }
+    }
+
+    ghCore.info(`Auto update complete, ${updated} pull request(s) that point to base branch '${baseBranch}' were updated.`);
+  }
+
+  async handlePullRequest() {
+    const { action } = this.eventData; 
+
+    ghCore.info(`Handling pull_request event triggered by action '${action}'`);
+    
+    const isUpdated = await this.update(this.eventData.pull_request);
+    if (isUpdated) {
+      ghCore.info(
+        `Auto update complete, pull request branch was updated with changes from the base branch.`
+      );
+    } else {
+      ghCore.info(`Auto update complete, no changes were made.`);
+    }
+  }
+
+  async update(pull) {
+    const { ref } = pull.head;
+    ghCore.info(`Evaluating pull request #${pull.number}...`);
+
+    if (pull.merged === true) {
+      ghCore.warning(` > Skipping pull request, already merged.`);
+      return false;
+    }
+    if (pull.state !== 'open') {
+      ghCore.warning(
+        ` > Skipping pull request, no longer open (current state: ${pull.state}).`
+      );
+      return false;
+    }
+
+    const prNeedsUpdate = await this.prNeedsUpdate(pull);
+    if (!prNeedsUpdate) {
+      return false;
+    }
+
+    const baseRef = pull.base.ref;
+    const headRef = pull.head.ref;
+    ghCore.info(` > Updating branch '${ref}' on pull request #${pull.number} with changes from ref '${baseRef}'.`);
+    const mergeResp = await this.octokit.repos.merge({
+      owner: pull.head.repo.owner.login,
+      repo: pull.head.repo.name,
+      // We want to merge the base branch into this one.
+      base: headRef,
+      head: baseRef,
+    });
+
+    // See https://developer.github.com/v3/repos/merging/#perform-a-merge
+    const status = mergeResp.status;
+    if (status === 200) {
+      ghCore.info(
+        ` > Branch update succesful, new branch HEAD: ${mergeResp.data.sha}.`
+      );
+    } else if (status === 204) {
+      ghCore.info(
+        ` > Branch update not required, branch is already up-to-date.`
+      );
+    }
+
+    return true;
+  }
+
+  async prNeedsUpdate(pull) {
+    const { data: comparison } = await this.octokit.repos.compareCommits({
+      owner: pull.head.repo.owner.login,
+      repo: pull.head.repo.name,
+      // This base->head, head->base logic is intentional, we want
+      // to see what would happen if we merged the base into head not
+      // vice-versa.
+      base: pull.head.label,
+      head: pull.base.label,
+    });
+
+    return comparison.behind_by > 0;
   }
 }
 

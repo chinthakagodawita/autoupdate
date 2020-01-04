47c0072f21a39247afd8a61a329347c7164cb571
Chinthaka Godawita <chin.godawita@me.com>
2020-01-04 12:43:27 +0000

    Debugging.

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 const process = require('process');
+const fs = require('fs');
+
+const ghCore = require('@actions/core');
 
 const AutoUpdater = require('../src/autoupdater.js');
 
 async function main() {
-  updater = new AutoUpdater({
-    githubToken: process.env['GITHUB_TOKEN'],
-    pullRequest: 'PR-1',
-    repository: 'chinthakagodawita/autoupdate-action',
-  });
-  await updater.run();
+  const eventPath = process.env['GITHUB_EVENT_PATH'];
+
+  const eventData = JSON.parse(fs.readSync(eventPath, 'utf8'));
+  
+  ghCore.debug(eventData);
+
+  // updater = new AutoUpdater({
+  //   githubToken: process.env['GITHUB_TOKEN'],
+  //   pullRequest: process.env[],
+  //   repository: process.env['GITHUB_REPOSITORY'],
+  // });
+  // await updater.run();
 }
 
 if (require.main === module) {
   main().catch((e) => {
     process.exitCode = 1;
-    console.log(e);
+    ghCore.setFailed(e.message);
   });
 }
